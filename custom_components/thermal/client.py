13fb4bc92b4c5a0c65a6e53b0b26efb02e720997
Will Freeman <6641284+TheRealWaldo@users.noreply.github.com>
2021-06-24 20:43:05 +0000

    fix: backwards compatibility with raw sensors

--- custom_components/thermal/client.py
+++ custom_components/thermal/client.py
@@ -2,6 +2,8 @@
 
 import requests
 
+from urllib.parse import urljoin
+
 
 class Client(object):
     def __init__(self, base_url, verify_ssl=False):
@@ -11,7 +13,10 @@ class Client(object):
 
     def get_raw(self):
         response = requests.get(
-            self._base_url, stream=True, timeout=self._timeout, verify=self._verify_ssl
+            urljoin(self._base_url, "raw"),
+            stream=True,
+            timeout=self._timeout,
+            verify=self._verify_ssl,
         )
         response.raise_for_status()
         return list(map(lambda x: float(x), response.json()["data"].split(",")))
